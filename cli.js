e1c44853256e1bb4bfb1019cf00b9febfdcdf8ce
Henry <henry@kirkness.com>
2018-03-25 21:33:25 +0100

    CHanged CWD

--- cli.js
+++ cli.js
@@ -2,8 +2,7 @@
 const childProcess = require('child_process');
 const path = require('path');
 
-process.chdir('../../');
-const pathToConfig = path.resolve(__dirname, './node_modules/expobook/expobook-app.json');
+const pathToConfig = path.resolve(process.cwd(), './node_modules/expobook/expobook-app.json');
 
 childProcess.execSync(`exp start --lan --ios --config ${pathToConfig}`, {
   stdio: [process.stdin, process.stdout, process.stderr],
