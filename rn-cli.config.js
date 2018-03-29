8e6d4526c0504a3a6713711c0014b7ea677ba89f
Henry <henry@kirkness.com>
2018-03-29 10:26:56 +0100

    Fix rn-cli config

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,15 +1,9 @@
 const path = require('path');
 const metroBundler = require('metro-bundler');
-console.log(
-  'react-ext',
-  path.resolve(__dirname, '../../node_modules/react-native')
-);
+
 const config = {
   extraNodeModules: {
-    'react-native-ext': path.resolve(
-      __dirname,
-      '../../node_modules/react-native'
-    ),
+    'react-native': path.resolve(__dirname, '../../node_modules/react-native'),
   },
   // getBlacklistRE() {
   //   return metroBundler.createBlacklist([/node_modules\/react-native\/.*/]);
