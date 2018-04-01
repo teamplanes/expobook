139f1f3966139aaea54e7f8096d10a2656491e55
Henry <henry@kirkness.com>
2018-04-01 10:15:52 +0100

    Added setup commands

--- cli.js
+++ cli.js
@@ -5,6 +5,16 @@ const path = require('path');
 process.chdir(__dirname);
 const pathToConfig = path.resolve(__dirname, './expobook-app.json');
 
+// Install all node modules
+console.log('Installing expobook dependencies...');
+childProcess.execSync(`npm i`, {
+  stdio: [null, null, process.stderr],
+});
+// Rename ../react-native to ../_react-native
+childProcess.execSync(`mv ../react-native ../_react-native`, {
+  stdio: [process.stdin, process.stdout, process.stderr],
+});
+
 childProcess.execSync(`exp start --lan --ios --config ${pathToConfig}`, {
   stdio: [process.stdin, process.stdout, process.stderr],
 });
