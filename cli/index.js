40f588dc8cb797bdcc7459dbb5602676bab91eeb
Henry <henry@kirkness.com>
2018-04-01 16:22:38 +0100

    Added exp as dep

--- cli/index.js
+++ cli/index.js
@@ -9,16 +9,22 @@ const getRel = file => path.resolve(__dirname, file);
 process.chdir(getRel(__dirname, '../'));
 const pathToConfig = getRel('../expobook-app.json');
 
-// eslint-disable-next-line no-console
-console.log('Installing expobook dependencies...');
-childProcess.execSync('pwd && npm i', {
-  stdio: [process.stdin, process.stdout, process.stderr],
-});
+if (!fs.existsSync(getRel('../node_modules'))) {
+  // eslint-disable-next-line no-console
+  console.log('Installing expobook dependencies...');
+  childProcess.execSync('pwd && npm i', {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+}
+
 // Rename ../react-native to ../_react-native
 if (fs.existsSync(getRel('../../react-native'))) {
   fs.renameSync(getRel('../../react-native'), getRel('../../_react-native'));
 }
 
-childProcess.execSync(`cd ../ && exp start --lan --ios --config ${pathToConfig}`, {
-  stdio: [process.stdin, process.stdout, process.stderr],
-});
+childProcess.execSync(
+  `cd ../ && ./node_modules/.bin/exp start --lan --ios --config ${pathToConfig}`,
+  {
+    stdio: [process.stdin, process.stdout, process.stderr],
+  },
+);
