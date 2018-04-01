cbbbf867c501af2f83c28380e2ee1dd9fda9b417
Henry <henry@kirkness.com>
2018-04-01 10:57:59 +0100

    Use node fs to remame files

--- cli/handle-exit.js
+++ cli/handle-exit.js
@@ -1,22 +1,19 @@
-const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-process.stdin.resume();
-
-function exitHandler(options, err) {
-  childProcess.execSync(`mv ../_react-native ../react-native`, {
-    stdio: [process.stdin, process.stdout, process.stderr],
-  });
+function exitHandler() {
+  if (fs.existsSync(path.resolve(__dirname, '../_react-native'))) {
+    fs.renameSync(path.resolve(
+      __dirname,
+      '../_react-native',
+      path.resolve(__dirname, '../react-native'),
+    ));
+  }
 }
 
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.stdin.resume();
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
--- cli/index.js
+++ cli/index.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 const childProcess = require('child_process');
 const path = require('path');
+const fs = require('fs');
 require('./handle-exit');
 
 process.chdir(__dirname);
-const pathToConfig = path.resolve(__dirname, './expobook-app.json');
+const getRel = file => path.resolve(__dirname, file);
+const pathToConfig = getRel('./expobook-app.json');
 
-// Install all node modules
+// eslint-disable-next-line no-console
 console.log('Installing expobook dependencies...');
-childProcess.execSync(`npm i`, {
+childProcess.execSync('npm i', {
   stdio: [null, null, process.stderr],
 });
 // Rename ../react-native to ../_react-native
-childProcess.execSync(`mv ../react-native ../_react-native`, {
-  stdio: [process.stdin, process.stdout, process.stderr],
-});
+if (fs.existsSync(getRel('../react-native'))) {
+  fs.renameSync(getRel('../react-native'), getRel('../_react-native'));
+}
+
 
 childProcess.execSync(`exp start --lan --ios --config ${pathToConfig}`, {
   stdio: [process.stdin, process.stdout, process.stderr],
