2d8f037e9ce86c18ecd5e388912fc2a8fdb1d129
Henry <henry@kirkness.com>
2018-04-01 11:24:49 +0100

    Debug

--- cli/index.js
+++ cli/index.js
@@ -7,6 +7,7 @@ require('./handle-exit');
 const getRel = file => path.resolve(__dirname, file);
 
 process.chdir(getRel(__dirname, '../'));
+childProcess.execSync('../');
 const pathToConfig = getRel('../expobook-app.json');
 
 // eslint-disable-next-line no-console
