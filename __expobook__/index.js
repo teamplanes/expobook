ae49282e704909924102b0ead8827e3eaa5011f7
Richard Palmer <rich@rdjpalmer.com>
2018-10-25 16:10:10 +0100

    Fix react-navigation deprecation warning (fixes #8)

--- __expobook__/index.js
+++ __expobook__/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StatusBar } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import React from "react";
+import PropTypes from "prop-types";
+import { StatusBar } from "react-native";
+import { createStackNavigator } from "react-navigation";
 
-import StateRestore from './components/state-restore';
-import StateSync from './components/state-sync';
-import ComponentList from './components/component-list';
+import StateRestore from "./components/state-restore";
+import StateSync from "./components/state-sync";
+import ComponentList from "./components/component-list";
 
-export const ROUTE_NAME_KEY = 'ROUTE_NAME';
+export const ROUTE_NAME_KEY = "ROUTE_NAME";
 
 const NEED_SAVE_STATE = __DEV__;
 
-const App = (props) => {
-  const Navigator = StackNavigator({
+const App = props => {
+  const Navigator = createStackNavigator({
     Home: {
-      screen: (screenProps) => {
+      screen: screenProps => {
         if (NEED_SAVE_STATE) {
           return (
             <StateRestore
@@ -22,17 +22,25 @@ const App = (props) => {
               currentRouteName={screenProps.navigation.state.routeName}
             >
               <StateSync addListener={screenProps.navigation.addListener}>
-                <ComponentList navigate={screenProps.navigation.navigate} {...props} />
+                <ComponentList
+                  navigate={screenProps.navigation.navigate}
+                  {...props}
+                />
               </StateSync>
             </StateRestore>
           );
         }
 
-        return <ComponentList navigate={screenProps.navigation.navigate} {...props} />;
+        return (
+          <ComponentList
+            navigate={screenProps.navigation.navigate}
+            {...props}
+          />
+        );
       },
       navigationOptions: () => ({
-        title: '📚',
-      }),
+        title: "📚"
+      })
     },
     ...Object.keys(props.components).reduce(
       (cur, next) => ({
@@ -40,27 +48,27 @@ const App = (props) => {
         [`Component:${next}`]: {
           screen: NEED_SAVE_STATE
             ? screenProps => (
-              <StateSync addListener={screenProps.navigation.addListener}>
-                {React.createElement(props.components[next])}
-              </StateSync>
-            )
+                <StateSync addListener={screenProps.navigation.addListener}>
+                  {React.createElement(props.components[next])}
+                </StateSync>
+              )
             : props.components[next],
 
           navigationOptions: () => ({
-            title: `${next}`,
-          }),
-        },
+            title: `${next}`
+          })
+        }
       }),
-      {},
-    ),
+      {}
+    )
   });
-  StatusBar.setBarStyle('dark-content');
+  StatusBar.setBarStyle("dark-content");
   return <Navigator />;
 };
 
 App.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  components: PropTypes.object.isRequired,
+  components: PropTypes.object.isRequired
 };
 
 export default components => () => <App components={components} />;
